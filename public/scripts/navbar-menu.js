f7c2f3ac3bd6cb4884735ca7db7423ac3d97883e
Luis G. Villegas <luisguve@gmail.com>
2021-07-27 15:03:56 -0300

    Vuelta a la version sin metamask

--- public/scripts/navbar-menu.js
+++ public/scripts/navbar-menu.js
@@ -18,37 +18,3 @@ function cerrarMenu() {
   contenedorMenu.classList.remove("abierto")
   menu.classList.remove("abierto")
 }
-
-async function metamaskDetection() {
-  const provider = await detectEthereumProvider()
-
-  if (provider) {
-    handleEthereum("detectEthereumProvider")
-  } else {
-    // handle no provider
-    if (window.ethereum) {
-      handleEthereum("window");
-    } else {
-      window.addEventListener('ethereum#initialized', () => handleEthereum("evento"), {
-        once: true,
-      });
-
-      // If the event is not dispatched by the end of the timeout,
-      // the user probably doesn't have MetaMask installed.
-      setTimeout(() => handleEthereum("timeout"), 3000); // 3 seconds
-    }
-  }
-}
-
-metamaskDetection()
-
-
-function handleEthereum(from) {
-  const { ethereum } = window;
-  if (ethereum && ethereum.isMetaMask) {
-    alert('MetaMask detectado desde ' + from);
-    // Access the decentralized web!
-  } else {
-    alert('Please install MetaMask!');
-  }
-}
